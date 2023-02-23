38b7ca9e5ffe8198676e4746eab33ef66fba7521
arvid <laveno@student.chalmers.se>
2023-02-23 16:38:48 +0100

    Progress on Projekt 1
    TODO
    	- code the function calculating Z
    	- code the conversion from u to something plotable
    	- Markow chain

--- Projekt1/DifferenceScheeme.py
+++ Projekt1/DifferenceScheeme.py
@@ -2,22 +2,43 @@ import numpy as np
 
 class Scheeme:
 
-    def __init__(self, function: str):
+    def __init__(self, function: str, time_partition : int, space_partition : int):
         
-        self.value = 0
-        self.A = 0
-        self.function = function
+        assert time_partition == space_partition, \
+            f"Expected equal sizes of the space partition and the time partition, got \
+                {time_partition}, {space_partition} "
 
+        self._tdim = time_partition
+        self._zdim = space_partition
+        self._u = np.zeros(shape=(self._tdim, self._zdim))
+        self._A = np.zeros(shape=(self._tdim, self._zdim))
+        self._d = 0
 
-    def update(self, S0, r, theta, K, T):
-        # TODO
-        pass
+    def build_sys(self):
+        
+        # TODO 
+        # increase definition of A to encompase A(-1/2*d)^-1A(1/2*d)
+        # there should also be a matrix for the coefficients a_ij
+        self._A = np.zeros(shape=(self._tdim, self._zdim))
+        self._A[np.eye(len(self._A), k=0, dtype='bool')] = 1 - 2*self._d
+        self._A[np.eye(len(self._A), k=1, dtype='bool')] = self._d
+        self._A[np.eye(len(self._A), k=-1, dtype='bool')] = self._d
+        return 0
 
+    def calculate(self, S, r, sigma, K, T):
 
-    def run(self, ite : int):
+        for t in range(self._tdim):
+            
+            if t == 0:
+                continue
 
-        for i in range(ite):
             # TODO 
-            pass
+            # provide calculation for aquiring Z
+            Z = ...
+            self._d = T * self._tdim**2/ (Z**2 * self._zdim)
+            self.build_sys()
+            self._u[1:,t+1] = self._A*self._u[1:,t]
+
+
 
 
--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from Projekt1 import DifferenceScheeme
 if __name__ == '__main__':
     
     f = "function_string"
+
+    arr = np.zeros(shape=(3,2))
+    print(arr.ndim)
     iterations = 100
-    scheeme = DifferenceScheeme.Scheeme(f)
-    scheeme.run(iterations)
+    scheeme = DifferenceScheeme.Scheeme(f,5,5)
+    # scheeme.run(iterations)
