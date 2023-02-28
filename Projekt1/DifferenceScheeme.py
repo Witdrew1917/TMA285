dab5f5cb61bffc09c470d9f698839111e87d75ac
arvid <laveno@student.chalmers.se>
2023-02-28 13:20:30 +0100

    Changes to how A is calculated
    In order to include aij-coefficients the way that A has been
    calculated has been changed. Also calculation for aij have been
    implemented.
    
    Todo
    	- Input boundary conditions
    	- Implement expression for Z
    	- Implement interpretation for u
    	- Create plots

--- Projekt1/DifferenceScheeme.py
+++ Projekt1/DifferenceScheeme.py
@@ -1,32 +1,51 @@
 import numpy as np
+import math
 
 class Scheeme:
 
-    def __init__(self, function: str, time_partition : int, space_partition : int):
-        
-        assert time_partition == space_partition, \
+    def __init__(self, function: str, time_length : int, space_width : int):
+
+        assert time_length == space_width, \
             f"Expected equal sizes of the space partition and the time partition, got \
-                {time_partition}, {space_partition} "
+                {time_length}, {space_width} "
 
-        self._tdim = time_partition
-        self._zdim = space_partition
-        self._u = np.zeros(shape=(self._tdim, self._zdim))
-        self._A = np.zeros(shape=(self._tdim, self._zdim))
-        self._d = 0
+        self._zdim = space_width
+        self._tdim = time_length
+        self._U_list = []
+        self._A_plus = np.zeros(shape=(self._zdim, self._zdim))
+        self._A_minus = np.zeros(shape=(self._zdim, self._zdim))
+        self._a = np.zeros(shape=(1,self._zdim))[0]
+        self._u = np.zeros(shape=(self._zdim, 1))
+        self._d = 0 
 
-    def build_sys(self):
+    def populate_sys(self):
         
         # TODO 
         # increase definition of A to encompase A(-1/2*d)^-1A(1/2*d)
         # there should also be a matrix for the coefficients a_ij
-        self._A = np.zeros(shape=(self._tdim, self._zdim))
-        self._A[np.eye(len(self._A), k=0, dtype='bool')] = 1 - 2*self._d
-        self._A[np.eye(len(self._A), k=1, dtype='bool')] = self._d
-        self._A[np.eye(len(self._A), k=-1, dtype='bool')] = self._d
+
+        temp = np.zeros(shape=(self._zdim, self._zdim))
+        temp += np.diag(-1*self._a, 0)
+        temp += np.diag(self._a[1:], -1)
+        temp += np.diag(-1*self._a[:(len(self._a)-1)], 1)
+        temp *= self._d
+        self._A_plus = temp.copy()
+        self._A_minus = -temp.copy()
+        self._A_plus += np.diag(np.ones(self._zdim),0) 
+        self._A_minus += np.diag(np.ones(self._zdim),0) 
+
+        return 0
+    
+    def create_boundry(self):
+        
+
+        
         return 0
 
-    def calculate(self, S, r, sigma, K, T):
 
+    def calculate(self, S:float, r:float, sigma:float, K:float, T:float):
+        
+        time = 0
         for t in range(self._tdim):
             
             if t == 0:
@@ -34,11 +53,22 @@ class Scheeme:
 
             # TODO 
             # provide calculation for aquiring Z
-            Z = ...
-            self._d = T * self._tdim**2/ (Z**2 * self._zdim)
-            self.build_sys()
-            self._u[1:,t+1] = self._A*self._u[1:,t]
-
+            # Z = ...
+            Z = np.random.rand(self._zdim) # temporary assignment
+            self._d = T * self._zdim**2/ (Z**2 * self._tdim)
+            gamma = (1 - math.exp(-r*time))/(r*T)*np.ones(self._zdim)
+            self._a = (sigma**2 / 2) * (gamma - Z)
+            self.populate_sys()
+            # self._u[1:,t+1] = self._A*self._u[1:,t]   
+            time += (T / self._tdim)
+        
+        # print(Z)
+        # print(gamma)
+        # print(gamma - Z)
+        # print(self._d)
+        # print(self._a)
+        # print(self._A_plus)
+        # print(self._A_minus)
 
 
 
--- main.py
+++ main.py
@@ -6,8 +6,6 @@ if __name__ == '__main__':
     
     f = "function_string"
 
-    arr = np.zeros(shape=(3,2))
-    print(arr.ndim)
-    iterations = 100
     scheeme = DifferenceScheeme.Scheeme(f,5,5)
+    scheeme.calculate(0,1,1,0,1)
     # scheeme.run(iterations)
