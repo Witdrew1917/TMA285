371d4512744036022db5b4bd17688c26534d46f5
arvid <laveno@student.chalmers.se>
2023-03-01 14:12:00 +0100

    Fix implementation of partitioning
    d is now correctly dependent on each partition of z.

--- Projekt1/DifferenceScheeme.py
+++ Projekt1/DifferenceScheeme.py
@@ -19,7 +19,7 @@ class Scheeme:
         self._a = np.zeros(shape=(1,self._zdim))[0]
         self._u = np.zeros(shape=(self._zdim, 1))
         # self._U_list.append(self._u.copy())
-        self._d = []
+        self._d = np.zeros(self._zdim)
         self._dt = dt
 
     def populate_sys(self):
@@ -32,7 +32,7 @@ class Scheeme:
         temp += np.diag(-1*self._a, 0)
         temp += np.diag(self._a[1:], -1)
         temp += np.diag(-1*self._a[:(len(self._a)-1)], 1)
-        temp *= self._d
+        temp *= (np.ones(shape=(len(self._d),len(self._d))) * self._d).T
         self._A_plus = temp.copy()
         self._A_minus = -temp.copy()
         self._A_plus += np.diag(np.ones(self._zdim),0) 
@@ -45,7 +45,7 @@ class Scheeme:
         
         sigma = self._dt
         W = np.random.rand(len(t))
-        W = 1/(np.sqrt(2*np.pi*(sigma**2)))*np.exp(-1/2*(W/sigma)**2)
+        W = 1/(np.sqrt(2*np.pi*(sigma**2)))*np.exp(-1/2*(W/sigma)**2) # this might not be correct
         
         self._S = S0*np.exp((r - sigma**2/2)*t + sigma*W)
 
@@ -54,20 +54,16 @@ class Scheeme:
     def partition_z(self, S0:float, r:float, K:float, T:float):
         
         # partition t uniformly
-        t = np.linspace(0, T, self._tdim)
+        t = np.linspace(0, T, self._tdim + 1)
         self.price_calulation(S0, r, t)
-        Q = []
-        
-        print(len(t))
-        print(len(self._S))
-
+        Q = np.zeros(len(t)).T
         for i in range(len(t)):
             Q[i] = sum(self._dt*self._S[:(i+1)])
         
         Z = []
         Z = 1/(r*T)*(1 - np.exp(-r*(T - t))) + np.matmul(np.exp(-r*(T - t))/self._S,(Q/T - K)) 
         Z.sort()
-        dz = []
+        dz = np.zeros(len(Z) - 1)
         for i in range(len(Z) - 1):
             dz[i] = Z[i + 1] - Z[i]
 
@@ -90,23 +86,14 @@ class Scheeme:
             Z = np.random.rand(self._zdim) # temporary assignment
             
             # should be vector, see equation in chat
-            self._d = T * self._zdim**2/ (Z**2 * self._tdim)
-
+            self._d = self._dt*np.ones(len(dz))/(dz**2)
             gamma = (1 - math.exp(-r*time))/(r*T)*np.ones(self._zdim)
             self._a = (sigma**2 / 2) * (gamma - Z)
             self.populate_sys()
             self._u = np.matmul(np.linalg.inv(self._A_plus),np.matmul(self._A_minus,self._u))
             self._U_list.append(self._u.copy())   
-            time += (T / self._tdim)
-        
-        # print(Z)
-        # print(gamma)
-        # print(gamma - Z)
-        # print(self._d)
-        # print(self._a)
-        print(self._A_plus)
-        print(self._A_minus)
-
+            time += self._dt
+       
     def plot(self, T:float, Zmax:float):
 
         fig = plt.figure()
